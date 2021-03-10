24dfe27eea4f06e11ac805ab84797dbbd3ca58f3
safwankdb <kdbeatbox@gmail.com>
2021-03-10 22:07:17 +0530

    cleanup FR

--- fletcher_reeves.py
+++ fletcher_reeves.py
@@ -14,33 +14,39 @@ def test_hessian(X):
     return 2 * np.eye(3)
 
 
-def newtons_method(grad, hessian, X_0, S, ε=1e-5):
+def fletcher_reeves(J, grad, hessian, X_0=np.zeros(3), ε_a=1e-5, ε_r=1e-5, ε_g=1e-5):
+    """
+    Fletcher Reeves Algorithm
+    """
+
+    def newtons_method(X_0, S, ε=1e-5):
+        """
+        Line Search using Modified Newton's Method
+        """
+        X = X_0
+        k = 0
+        α = 0
+        while k < 10:
+            T = hessian(X) @ S
+            G = grad(X)
+            α += -(G @ T.T) / (T @ T.T) / 2
+            if np.linalg.norm(α*S, 2) < ε:
+                break
+            X = X + α * S
+            k += 1
+        return α
+
     X = X_0
     k = 0
-    α = 0
-    while k < 10:
-        T = hessian(X) @ S
-        G = grad(X)
-        α += -(G @ T.T) / (T @ T.T) / 2
-        if np.linalg.norm(α*S, 2) < ε:
-            break
-        X = X + α * S
-        k += 1
-    return α
-
-
-def fletcher_reeves(J, grad, X_0=np.zeros(3), ε_a=1e-5, ε_r=1e-5, ε_g=1e-5):
-    X = X_0
-    k = 1
     while True:
         G = grad(X)
-        if k == 1:
+        if k == 0:
             S = -G / np.linalg.norm(G, 2)
         else:
             β = G.T @ G / (G_old.T @ G_old)
             S = -G / np.linalg.norm(G, 2) + β*S_old
         G_old, S_old = G, S
-        α = newtons_method(test_grad, test_hessian, X, S)
+        α = newtons_method(X, S)
         X_old = X
         X += α * S
         k += 1
@@ -51,5 +57,5 @@ def fletcher_reeves(J, grad, X_0=np.zeros(3), ε_a=1e-5, ε_r=1e-5, ε_g=1e-5):
 
 
 if __name__ == "__main__":
-    X = fletcher_reeves(test_obj, test_grad)
+    X = fletcher_reeves(test_obj, test_grad, test_hessian)
     print(X)
