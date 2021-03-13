51ba033a5c4dccc7692f26e90352c7b8e794bad3
safwankdb <kdbeatbox@gmail.com>
2021-03-13 19:32:52 +0530

    Added the modification, switched to infinity norm

--- fletcher_reeves.py
+++ fletcher_reeves.py
@@ -1,17 +1,19 @@
 import jax.numpy as np
 from jax import grad, hessian
+from numpy import random
 
 
 def rosen(x):
     """The Rosenbrock function"""
     return np.sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
 
-def newtons_method(grad, hessian, x, s):
+
+def newtons_method(grad, hessian, x, s, n):
     """
     Line Search using Modified Newton's Method
     """
     α = 0
-    for _ in range(1):
+    for _ in range(n):
         G = grad(x)
         T = hessian(x) @ s
         dα = -(G @ T.T) / (T @ T.T)
@@ -19,7 +21,8 @@ def newtons_method(grad, hessian, x, s):
         x += dα * s
     return α
 
-def fletcher_reeves(J, grad, hessian, X_0=np.zeros(3), ε_a=1e-5, ε_r=1e-5, ε_g=1e-4):
+
+def fletcher_reeves(J, grad, hessian, X_0, n_iter=1, ε_a=1e-6, ε_r=1e-6, ε_g=1e-6, verbose=False):
     """
     Fletcher Reeves Algorithm
     """
@@ -27,21 +30,32 @@ def fletcher_reeves(J, grad, hessian, X_0=np.zeros(3), ε_a=1e-5, ε_r=1e-5, ε_
     k = 0
     while True:
         G = grad(X)
+        if verbose:
+            print(k, X)
+            print(np.linalg.norm(G, ord=float('inf')))
         if k == 0:
-            S = -G / np.linalg.norm(G)
+            S = -G / np.linalg.norm(G, ord=2)
         else:
-            β = G.T @ G / (G_old.T @ G_old)
-            S = -G / np.linalg.norm(G) + β*S_old
+            β = G.T @ (G-G_old) / (G_old.T @ G_old)
+            S = -G / np.linalg.norm(G, ord=float('inf')) + β*S_old
         X_old, G_old, S_old = X, G, S
-        X += newtons_method(grad, hessian, X, S) * S
+        X += newtons_method(grad, hessian, X, S, n_iter) * S
         k += 1
         if abs(J(X)-J(X_old)) <= ε_a + ε_r*abs(J(X_old))\
-                and np.linalg.norm(G) <= ε_g:
+                and np.linalg.norm(G, ord=float('inf')) <= ε_g:
             break
+        if np.abs(X-X_old).max() < 1e-6 or k > 200:
+            k = 0
+            X = random.rand(len(X))
     return X
 
 
 if __name__ == "__main__":
-    X_0 = np.array([2.0,2.0, 0.5])
-    X = fletcher_reeves(rosen, grad(rosen), hessian(rosen), X_0)
+    X_0 = np.array([2.0, 2.0, 0.5])
+    X = fletcher_reeves(rosen,
+                        grad(rosen),
+                        hessian(rosen),
+                        X_0, ε_a=1e-5,
+                        ε_r=1e-5, ε_g=1e-4,
+                        verbose=True)
     print(X)
