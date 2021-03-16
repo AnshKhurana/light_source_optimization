e07e0531e7ac4a31441a8be30452639bd3759630
AnshKhurana <anshkhurana1507@gmail.com>
2021-03-16 12:11:31 +0530

    Added code for 3D mesh visualization

--- environment.py
+++ environment.py
@@ -1,5 +1,8 @@
 """Environment definition."""
+import matplotlib
+matplotlib.use('tkagg')
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import jax.numpy as np
 from scipy.optimize import minimize
 from jax import grad, hessian
@@ -11,6 +14,7 @@ class Room:
     def __init__(self, l, b, h,
                  mesh_resolution=10, mesh_type='horizontal', plane_a=None,
                  plane_b=None, plane_c=None, plane_d=None, plane_height=None,
+                 obj_weight=None,
                  objective_type='simple_min'):
         """Setup environment variables.
 
@@ -34,6 +38,7 @@ class Room:
         self.objective_type = objective_type
         self.mesh_x, self.mesh_y, self.mesh_z = self.generate_mesh()
         self.J = self.objective_function
+        self.obj_weight = obj_weight
         self.gradient = grad(self.objective_function)
         self.hessian = hessian(self.objective_function)
 
@@ -62,7 +67,21 @@ class Room:
         """Creates a 3D plot of the room, with placed bulbs and the
         target plane.
         """
-        pass
+        if bulb_positions.ndim == 1:
+            bulb_positions = self.to_pos(bulb_positions)
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.set_xlabel('l')
+        ax.set_ylabel('b')
+        ax.set_zlabel('h')
+        ax.set_xlim(0, self.l)
+        ax.set_ylim(0, self.b)
+        ax.set_zlim(0, self.h)
+
+        ax.scatter(bulb_positions[:, 0], bulb_positions[:, 1],
+                   bulb_positions[:, 2], marker='^', c='r')
+        surf = ax.plot_surface(self.mesh_x, self.mesh_y, self.mesh_z)
+        plt.show()
 
     def return_grid(self):
         return self.mesh_x, self.mesh_y, self.mesh_z
@@ -95,6 +114,9 @@ class Room:
             obj = np.std(I)
         elif self.objective_function == 'simple_penalty_min':
             obj = -1*np.min(I) +(bulb_pos[:, 0]) 
+        elif self.objective_function == 'simple_combined':
+            assert self.obj_weight is not None
+            obj = self.obj_weight * np.std(I) + (self.obj_weight - 1) * np.min(I)
         else:
             raise NotImplementedError(
                 f'Objective function {self.objective_type} is not defined.')
@@ -194,6 +216,7 @@ class Roof:
                 f'Objective function {self.objective_type} is not defined.')
         return obj
 
+
 room = Room(10, 15, 20, plane_height=5, objective_type='simple_min')
 
 
@@ -207,9 +230,9 @@ def room_scipy(bulb_position):
 def test_scipy():
     bulb_pos = onp.array([[6, 8, 10], [2, 1, 3], [10, 10, 15]])
     print("Init bulb pos: \n", bulb_pos)
-    
+   
     res = minimize(room_scipy, bulb_pos.ravel(), method='nelder-mead',
-               options={'xatol': 1e-10, 'disp': True})
+                   options={'xatol': 1e-10, 'disp': True})
 
     print(np.reshape(res.x, (-1, 3)))
 
@@ -219,6 +242,7 @@ if __name__ == '__main__':
     bulb_pos = onp.random.rand(5 * 3) * 10
     print("Init bulb pos: \n", bulb_pos)
     grid_x, grid_y, grid_z = room.return_grid()
+    room.show_plane(bulb_pos)
     print("Grid Shape :")
     print(grid_x.shape, grid_y.shape, grid_z.shape)
     print("Obj at initial bulb position: ", room.objective_function(bulb_pos))
