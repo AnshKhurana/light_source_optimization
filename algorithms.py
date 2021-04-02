e3f67e566ce4b3777679d29c0f7465dd6d998d4a
satvikmashkaria <satvikmashkaria@gmail.com>
2021-04-02 13:07:29 +0530

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,7 @@
 """Implementation of optimization algorithms."""
 import jax.numpy as np
 import numpy as np
+import scipy.optimize as sopt
 
 def test_obj(X):
     return np.linalg.norm(X - np.array([1.0, 2.0, 3.0]))**2
@@ -9,7 +10,7 @@ def test_grad(X):
     return 2*(X - np.array([1.0, 2.0, 3.0]))
 
 def steepest_descent():
-    X_0 = np.array([0.0, 0.0, 0.0])
+    X_0 = np.array([10.0, -3.0, 0.0])
     num_itr = 128
     X = X_0
     alpha_0 = 1e-2 * np.ones(3)
@@ -29,13 +30,15 @@ def steepest_descent():
         # if np.abs(J - prev_J) > eps_a + eps_r * np.abs(prev_J) or np.linalg.norm(G) > eps_g:
         #     print("Converged!")
         #     break
-        alpha = prev_alpha * (prev_G.T @ prev_G)/(G.T @ G)
+        def f1d(alpha):
+            return test_obj(X + alpha*S)
+        alpha = sopt.golden(f1d)
+        print("Alpha: ", alpha)
+        # alpha = prev_alpha * (prev_G.T @ prev_G)/(G.T @ G)
         X += alpha*S
         prev_alpha = alpha
         prev_G = G
         prev_J = J
-
-
 
     pass
 
