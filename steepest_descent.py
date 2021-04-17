adc4fa5c4cf6b8e8884c0079dda993ab1f10abf7
satvikmashkaria <satvikmashkaria@gmail.com>
2021-04-17 23:35:58 +0530

    Update steepest_descent.py

--- steepest_descent.py
+++ steepest_descent.py
@@ -1,5 +1,8 @@
 """Implementation of steepest descent algorithm."""
-import numpy as np
+# import numpy as np
+from jax import grad, hessian
+import jax.numpy as np
+
 
 
 def test_obj(X):
@@ -8,6 +11,10 @@ def test_obj(X):
 
 def test_grad(X):
     return 2*(X - np.array([1.0, 2.0, 3.0]))
+
+def rosen(x):
+    """The Rosenbrock function"""
+    return np.sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
 
 
 def steepest_descent(X_0, alpha_0, obj_fn=test_obj, grad_fn=test_grad,
@@ -45,7 +52,11 @@ def steepest_descent(X_0, alpha_0, obj_fn=test_obj, grad_fn=test_grad,
 if __name__ == "__main__":
 
     X0 = np.array([1, 1, 1])
-    X_0 = np.array([0.0, 0.0, 0.0])
-    alpha_0 = 1e-2 * np.ones(3)
+    # X_0 = np.array([0.0,  0.0, 0.0])
+    X_0 = np.array([0.0, 0.0])
+    alpha_0 = 1e-2 * np.ones(2)
     print(np.linalg.norm(X0)**2)
-    steepest_descent(X_0, alpha_0, num_itr=2000)
+    # steepest_descent(X_0, alpha_0, num_itr=2000)
+
+    X = steepest_descent(X_0, alpha_0, obj_fn=rosen,
+                        grad_fn=grad(rosen), num_itr=2000)
